df7aebf6d46bfd4e45329c82d8204e871cf57989
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2023-08-17 23:40:51 +0900

    ダミーイメージ追加、画像クリックでconsole出力

--- choco/src/App.tsx
+++ choco/src/App.tsx
@@ -1,14 +1,24 @@
-//import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "./App.css";
-//import imageList from "./data/imageList.json";
+import imageList from "./data/imageList.json";
+import { useState } from "react";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const handleClick = () => {
+    setScore(score + 1);
+    console.log(score);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
       <p className="text-xl">上から降ってくるチョコミントをクリックしよう</p>
+      {imageList.map((item) => (
+        <img key={item.id} src={item.path} alt="" onClick={handleClick} />
+      ))}
     </div>
   );
 }
