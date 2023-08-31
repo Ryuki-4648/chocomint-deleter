9559e27914bcf12c3d4feb462acc82424b36dfc0
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2023-08-31 23:04:01 +0900

    スタイル調整、フォント指定

--- choco/src/App.tsx
+++ choco/src/App.tsx
@@ -16,24 +16,28 @@ function App() {
 
   return (
     <div className="App h-screen pt-16 pb-20 bg-color01">
-      <p className="text-white text-5xl font-semibold absolute right-6 top-2">
+      <p className="text-white text-5xl font-semibold absolute right-6 top-2 font01">
         {seconds}
       </p>
-      <div className="falling-container bg-white h-full relative overflow-hidden">
-        <p className="text-2xl font-bold text-center absolute top-5 left-1/2 -translate-x-1/2">
-          チョコミントをクリックして消していこう
-        </p>
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="falling-container bg-white h-full relative overflow-hidden text-gray-600">
+        {startText && (
+          <p className="text-xl text-center absolute top-8 left-1/2 -translate-x-1/2">
+            チョコミントをクリックして消していこう！
+          </p>
+        )}
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center w-full">
           {startText && (
-            <p className="text-color01 text-5xl font-bold">CHOCOMINT DELETER</p>
-          )}
-          {startText && (
-            <button
-              onClick={onClickStartButton}
-              className="text-color01 font-bold mt-4"
-            >
-              スタートする
-            </button>
+            <>
+              <p className="text-color01 text-6xl font-bold tracking-widest font01">
+                CHOCOMINT DELETER
+              </p>
+              <button
+                onClick={onClickStartButton}
+                className="text-color01 font-bold mt-8 border-color01 border border-solid w-60 py-2 text-lg"
+              >
+                スタートする
+              </button>
+            </>
           )}
         </div>
         {fallingImages.map((item) => (
@@ -55,17 +59,20 @@ function App() {
             {finishText}
           </p>
           {resetButton && (
-            <button className="text-xl" onClick={onClickResetButton}>
+            <button
+              className="text-xl border border-solid w-60 py-2 border-gray-300"
+              onClick={onClickResetButton}
+            >
               リベンジする
             </button>
           )}
         </div>
       </div>
       <div className="px-6 py-2 flex justify-center items-center">
-        <p className="text-white text-2xl font-semibold mr-3 tracking-wider">
+        <p className="text-white text-3xl font-semibold mr-3 tracking-widest font01">
           SCORE
         </p>
-        <p className="text-white text-6xl font-semibold">{score}</p>
+        <p className="text-white text-6xl font-semibold font01">{score}</p>
       </div>
     </div>
   );
